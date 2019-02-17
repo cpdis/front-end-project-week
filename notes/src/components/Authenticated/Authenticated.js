9ebc39e5e8559c13da195fadc69de037436067d8
Colin Dismuke <colin.dismuke@gmail.com>
2019-02-16 22:33:50 -0600

    Added Authenticated component

---
+++ notes/src/components/Authenticated/Authenticated.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import * as blockstack from "blockstack";
+
+// Authenticated routes are only available when signed in with Blockstack
+const Authenticated = ({ component, ...rest }) => {
+  const isAuthenticated = blockstack.isUserSignedIn();
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return isAuthenticated ? (
+          React.createElement(component, { ...props })
+        ) : (
+          <Redirect to="/logout" />
+        );
+      }}
+    />
+  );
+};
+
+export default Authenticated;
